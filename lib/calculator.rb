d027a51e6ad98a729be27fc0d4658865fdee55bb
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-11-16 19:13:12 -0500

    Second commit

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -9,16 +9,18 @@ class Calculator
       if arr.include? '.'
         return "Please give only whole numbers"
       else
-        ans = eval(input).to_s
+        ans = eval(input)
         return ans.to_i
       end
     end
   end
 
-puts "welcome to Calculator!"
-print "Please type your math problem: "
-input = gets.to_s
-answer = Calculator.count(input)
-puts answer
-
+  puts "Welcome to Calculator!"
+  puts "Hit Ctrl+c for exit."
+  loop do
+    print "Please type your math problem: "
+    input = gets.to_s
+    answer = Calculator.count(input)
+    puts answer
+  end
 end
